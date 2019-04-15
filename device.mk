f714a2d7780f90cc364640a070a0bdd8bb82c0ee
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-04-15 20:45:32 +0430

    gemini: add op-gallery

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ PRODUCT_COPY_FILES += \
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
+#Gallery
+$(call inherit-product-if-exists, vendor/oneplus/OpGallery/Android.mk)
+
 # ConfigPanel
 PRODUCT_PACKAGES += \
     ConfigPanel
