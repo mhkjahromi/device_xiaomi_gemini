d88cc4a05f892149d0315e3ca819ca28cfe78d96
mhkjahromi <m.h.k.jahromi@gmail.com>
2019-05-10 15:49:41 +0430

    gemini: Update fingerprint to 8.11.22

--- aosp_gemini.mk
+++ aosp_gemini.mk
@@ -43,10 +43,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="gemini" \
     PRODUCT_NAME="gemini" \
-    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAAMIFD release-keys"
+    PRIVATE_BUILD_DESC="gemini-user 8.0.0 OPR1.170623.032 8.11.22 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/V9.6.1.0.OAAMIFD:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/gemini/gemini:8.0.0/OPR1.170623.032/8.11.22:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
 
